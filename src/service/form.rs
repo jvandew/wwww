a15069da419a7f0b8e85813281876c0b843b9668
Jacob Van De Weert <vandeweertj@gmail.com>
2018-07-18 22:57:03 -0400

    disambiguate form string values from single array values

--- src/service/form.rs
+++ src/service/form.rs
@@ -3,6 +3,11 @@ use std::collections::HashMap;
 use std::str;
 use url::form_urlencoded;
 
+enum ParsingFormValue {
+    String(String),
+    Array(Vec<Option<String>>),
+}
+
 #[derive(Debug)]
 pub enum FormValue {
     String(String),
@@ -36,7 +41,7 @@ impl FormValue {
     pub fn parse_form_data(form_data: &[u8]) -> HashMap<String, FormValue> {
         // Parse everything into vectors for simplicity, and then do a final pass to convert
         // to the appropriate enum.
-        let initial_vector_map: HashMap<String, Vec<Option<String>>> = HashMap::new();
+        let initial_vector_map: HashMap<String, ParsingFormValue> = HashMap::new();
         // TODO(jacob): figure out how to cache this
         let array_regex = Regex::new(r"(?P<name>.+)\[(?P<index>\d+)\]").unwrap();
 
@@ -45,7 +50,9 @@ impl FormValue {
             .fold(
                 initial_vector_map,
                 |mut vector_map, (name, value)| {
-                    let (var_name, index, mut new_vector) = array_regex.captures(&name)
+                    let value1 = value.clone();
+                    let value2 = value.clone();
+                    let (var_name, new_value) = array_regex.captures(&name)
                         .and_then(|captures| {
                             captures.name("name")
                                 .map(|mat| mat.as_str())
@@ -56,42 +63,56 @@ impl FormValue {
                                         .map(|index| (var_name.to_string(), index))
                                 })
                         }).map_or_else(
-                            || {
-                                let mut new_values = Vec::with_capacity(1);
-                                new_values.resize(1, None);
-                                (name, 0, new_values)
-                            },
+                            || (name, ParsingFormValue::String(value)),
                             |(var_name, index)| {
-                                let new_vector = vector_map.get(&var_name).map_or_else(
+                                let new_parsing_value = vector_map.get(&var_name).map_or_else(
                                     || {
                                         let mut new_values = Vec::with_capacity(index + 1);
                                         new_values.resize(index + 1, None);
-                                        new_values
+                                        new_values[index] = Some(value1);
+                                        ParsingFormValue::Array(new_values)
                                     },
-                                    |values| {
-                                        let mut new_values = values.clone();
-                                        if new_values.len() <= index {
-                                            new_values.resize(index + 1, None);
-                                        };
-                                        new_values
+                                    |existing_value| {
+                                        match existing_value {
+                                            ParsingFormValue::String(_) => {
+                                                let mut new_values = Vec::with_capacity(index + 1);
+                                                new_values.resize(index + 1, None);
+                                                new_values[index] = Some(value2);
+                                                ParsingFormValue::Array(new_values)
+                                            },
+                                            ParsingFormValue::Array(values) => {
+                                                let mut new_values = values.clone();
+                                                if new_values.len() <= index {
+                                                    new_values.resize(index + 1, None);
+                                                };
+                                                new_values[index] = Some(value2);
+                                                ParsingFormValue::Array(new_values)
+                                            },
+                                        }
                                     },
                                 );
-                                (var_name, index, new_vector)
+                                (var_name, new_parsing_value)
                             },
                         );
 
-                    new_vector[index] = Some(value);
-                    vector_map.insert(var_name, new_vector);
+                    vector_map.insert(var_name, new_value);
                     vector_map
                 }
             );
 
-        vector_map.into_iter().flat_map(|(name, values)| {
-            let flattened_values = values.into_iter().flat_map(|value_opt| value_opt).collect::<Vec<String>>();
-            match &flattened_values[..] {
-                [] => None,  // should never happen with well-formed input
-                [single] => Some((name, FormValue::String(single.to_string()))),
-                many => Some((name, FormValue::Array(many.to_vec()))),
+        vector_map.into_iter().flat_map(|(name, value)| {
+            match value {
+                ParsingFormValue::String(string) => Some((name, FormValue::String(string))),
+                ParsingFormValue::Array(values) => {
+                    let flattened_values = values
+                        .into_iter()
+                        .flat_map(|value_opt| value_opt)
+                        .collect::<Vec<String>>();
+                    match &flattened_values[..] {
+                        [] => None, // should never happen with well-formed input
+                        valid => Some((name, FormValue::Array(valid.to_vec()))),
+                    }
+                }
             }
         }).collect()
     }
